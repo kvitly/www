fb80a00020f3ace47d5477f0bf6b21dcfce1f1d7
Yahor Kuryanovich <egor@kuryanovich.by>
2022-06-18 13:35:51 +0300

    feat: Обновлен подвал сайта

--- components/GlobalFooter.vue
+++ components/GlobalFooter.vue
@@ -1,25 +1,25 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <footer class="bg-white" aria-labelledby="footer-heading">
+  <footer class="relative" aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
       <div class="xl:grid xl:grid-cols-3 xl:gap-8">
         <div class="space-y-8 xl:col-span-1">
-          <img class="h-10" src="https://tailwindui.com/img/logos/workflow-mark-gray-300.svg" alt="Company name" />
-          <p class="text-gray-500 text-base">Making the world a better place through constructing elegant hierarchies.</p>
-          <div class="flex space-x-6">
-            <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
+          <img class="h-16" src="~/assets/kvitly.svg" alt="🌱kvitly" />
+          <p class="text-gray-500 text-base">{{footer.tagline}}</p>
+          <div class="flex items-center space-x-6">
+            <a v-for="item in footer.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
               <span class="sr-only">{{ item.name }}</span>
-              <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+              <component :is="item.icon" class="h-4 w-4" aria-hidden="true" />
             </a>
           </div>
         </div>
         <div class="mt-12 grid grid-cols-2 gap-8 xl:mt-0 xl:col-span-2">
           <div class="md:grid md:grid-cols-2 md:gap-8">
             <div>
-              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Solutions</h3>
+              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Возможности</h3>
               <ul role="list" class="mt-4 space-y-4">
-                <li v-for="item in navigation.solutions" :key="item.name">
+                <li v-for="item in footer.solutions" :key="item.name">
                   <a :href="item.href" class="text-base text-gray-500 hover:text-gray-900">
                     {{ item.name }}
                   </a>
@@ -27,9 +27,9 @@
               </ul>
             </div>
             <div class="mt-12 md:mt-0">
-              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Support</h3>
+              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">&nbsp;</h3>
               <ul role="list" class="mt-4 space-y-4">
-                <li v-for="item in navigation.support" :key="item.name">
+                <li v-for="item in footer.support" :key="item.name">
                   <a :href="item.href" class="text-base text-gray-500 hover:text-gray-900">
                     {{ item.name }}
                   </a>
@@ -39,9 +39,9 @@
           </div>
           <div class="md:grid md:grid-cols-2 md:gap-8">
             <div>
-              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Company</h3>
+              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Сервис</h3>
               <ul role="list" class="mt-4 space-y-4">
-                <li v-for="item in navigation.company" :key="item.name">
+                <li v-for="item in footer.company" :key="item.name">
                   <a :href="item.href" class="text-base text-gray-500 hover:text-gray-900">
                     {{ item.name }}
                   </a>
@@ -49,9 +49,9 @@
               </ul>
             </div>
             <div class="mt-12 md:mt-0">
-              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Legal</h3>
+              <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">Ресурсы</h3>
               <ul role="list" class="mt-4 space-y-4">
-                <li v-for="item in navigation.legal" :key="item.name">
+                <li v-for="item in footer.legal" :key="item.name">
                   <a :href="item.href" class="text-base text-gray-500 hover:text-gray-900">
                     {{ item.name }}
                   </a>
@@ -61,8 +61,9 @@
           </div>
         </div>
       </div>
-      <div class="mt-12 border-t border-gray-200 pt-8">
-        <p class="text-base text-gray-400 xl:text-center">&copy; 2020 Workflow, Inc. All rights reserved.</p>
+      <div class="mt-12 flex items-center px-8 justify-between border-t border-gray-200 pt-8">
+        <p class="text-md text-gray-400 xl:text-center">{{footer.copyright}}</p>
+        <img class="max-w-sm opacity-75" src="~/assets/payments-bw.png" />
       </div>
     </div>
   </footer>
@@ -71,41 +72,57 @@
 <script setup>
 import { defineComponent, h } from 'vue'
 
-const navigation = {
+const footer = {
+  tagline: "Making the world a better place through constructing elegant hierarchies.",
+  copyright: "© Сделано с 💚 в Беларуси, 2014—2022.",
   solutions: [
-    { name: 'Marketing', href: '#' },
-    { name: 'Analytics', href: '#' },
-    { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
+    { name: 'Веб-сайт', href: '/products/website' },
+    { name: 'Интернет-магазин', href: '/products/ecommerce' },
+    { name: 'Рассылки', href: '/products/email' },
+    { name: 'Интеграции', href: '/integrations' },
   ],
   support: [
-    { name: 'Pricing', href: '#' },
-    { name: 'Documentation', href: '#' },
-    { name: 'Guides', href: '#' },
-    { name: 'API Status', href: '#' },
+    { name: 'Лендинг', href: '/products/landing' },
+    { name: 'Платежи', href: '/products/payments' },
+    { name: 'CRM-система', href: '/products/crm' },
+    // { name: 'API Status', href: '#' },
   ],
   company: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Partners', href: '#' },
+    { name: 'Тарифы', href: '/plans' },
+    { name: 'Блог', href: '#' },
+    { name: 'О нас', href: '/about' },
+    // { name: 'Press', href: '#' },
+    // { name: 'Partners', href: '#' },
   ],
   legal: [
-    { name: 'Claim', href: '#' },
-    { name: 'Privacy', href: '#' },
-    { name: 'Terms', href: '#' },
+    { name: 'Справка', href: '#' },
+    { name: 'Вебинары', href: '#' },
+    { name: 'Партнерам', href: '#' },
   ],
   social: [
     {
       name: 'Facebook',
-      href: '#',
+      href: 'https://www.facebook.com/staronkaby',
       icon: defineComponent({
         render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+          h('svg', { fill: 'currentColor', viewBox: '0 0 16 16' }, [
             h('path', {
               'fill-rule': 'evenodd',
-              d: 'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z',
+              d: 'M6.02293,16L6,9H3V6h3V4c0-2.6992,1.67151-4,4.07938-4c1.15339,0,2.14468,0.08587,2.43356,0.12425v2.82082 l-1.66998,0.00076c-1.30953,0-1.56309,0.62227-1.56309,1.53541V6H13l-1,3H9.27986v7H6.02293z',
+              'clip-rule': 'evenodd',
+            }),
+          ]),
+      }),
+    },
+    {
+      name: 'VK',
+      href: 'https://vk.com/staronkaby',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 16 16' }, [
+            h('path', {
+              'fill-rule': 'evenodd',
+              d: 'M13.535,0H2.465C1.104,0,0,1.104,0,2.465v11.069 C0,14.896,1.104,16,2.465,16h11.069C14.896,16,16,14.896,16,13.535V2.465C16,1.104,14.896,0,13.535,0z M13.135,11.265h-1.346 c-0.439,0-0.494-0.32-1.3-1.09c-0.68-0.67-0.971-0.723-1.135-0.723c-0.147,0-0.247,0.11-0.247,0.366v1.025 c0,0.302-0.174,0.421-0.888,0.421c-1.172,0-2.472-0.723-3.461-2.06c-1.474-1.987-1.85-3.47-1.85-3.791 c0-0.174,0.128-0.293,0.302-0.293h1.199c0.311,0,0.43,0.092,0.54,0.421c0.549,1.611,1.529,3.031,1.941,3.031 c0.156,0,0.174-0.128,0.174-0.458V6.476c0-0.794-0.449-0.861-0.449-1.154c0-0.116,0.092-0.201,0.238-0.201h1.923 c0.256,0,0.293,0.092,0.293,0.403v2.225c0,0.254,0.052,0.348,0.165,0.348c0.14,0,0.269-0.085,0.522-0.366 c0.789-0.871,1.438-2.252,1.438-2.252c0.082-0.174,0.183-0.293,0.458-0.293c0.403,0,1.199,0,1.199,0 c0.238,0,0.339,0.128,0.293,0.357c-0.137,0.641-1.52,2.591-1.52,2.591c-0.128,0.201-0.183,0.311,0,0.504 c0.128,0.165,0.531,0.522,0.778,0.851c0.604,0.641,0.998,1.181,0.998,1.483C13.401,11.2,13.272,11.265,13.135,11.265z',
               'clip-rule': 'evenodd',
             }),
           ]),
@@ -113,53 +130,63 @@ const navigation = {
     },
     {
       name: 'Instagram',
-      href: '#',
+      href: 'https://www.instagram.com/staronkaby',
       icon: defineComponent({
         render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+          h('svg', { fill: 'currentColor', viewBox: '0 0 16 16' }, [
             h('path', {
               'fill-rule': 'evenodd',
-              d: 'M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z',
+              d: 'M8,12c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S10.206,12,8,12z M8,6C6.897,6,6,6.897,6,8 s0.897,2,2,2s2-0.897,2-2S9.103,6,8,6z',
               'clip-rule': 'evenodd',
+            }),
+            h('path', {
+              'fill-rule': 'evenodd',
+              d: 'M12,16H4c-2.056,0-4-1.944-4-4V4c0-2.056,1.944-4,4-4h8c2.056,0,4,1.944,4,4v8C16,14.056,14.056,16,12,16z M4,2C3.065,2,2,3.065,2,4v8c0,0.953,1.047,2,2,2h8c0.935,0,2-1.065,2-2V4c0-0.935-1.065-2-2-2H4z',
+              'clip-rule': 'evenodd',
+            }),
+            h('circle', {
+              cx: '12.145',
+              cy: '3.892',
+              r: '0.96',
             }),
           ]),
       }),
     },
     {
       name: 'Twitter',
-      href: '#',
+      href: 'https://twitter.com/staronkaby',
       icon: defineComponent({
         render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+          h('svg', { fill: 'currentColor', viewBox: '0 0 16 16' }, [
             h('path', {
-              d: 'M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84',
+              d: 'M16,3c-0.6,0.3-1.2,0.4-1.9,0.5c0.7-0.4,1.2-1,1.4-1.8c-0.6,0.4-1.3,0.6-2.1,0.8c-0.6-0.6-1.5-1-2.4-1 C9.3,1.5,7.8,3,7.8,4.8c0,0.3,0,0.5,0.1,0.7C5.2,5.4,2.7,4.1,1.1,2.1c-0.3,0.5-0.4,1-0.4,1.7c0,1.1,0.6,2.1,1.5,2.7 c-0.5,0-1-0.2-1.5-0.4c0,0,0,0,0,0c0,1.6,1.1,2.9,2.6,3.2C3,9.4,2.7,9.4,2.4,9.4c-0.2,0-0.4,0-0.6-0.1c0.4,1.3,1.6,2.3,3.1,2.3 c-1.1,0.9-2.5,1.4-4.1,1.4c-0.3,0-0.5,0-0.8,0c1.5,0.9,3.2,1.5,5,1.5c6,0,9.3-5,9.3-9.3c0-0.1,0-0.3,0-0.4C15,4.3,15.6,3.7,16,3z',
             }),
           ]),
       }),
     },
     {
-      name: 'GitHub',
-      href: '#',
+      name: 'Telegram',
+      href: 'https://t.me/menavita',
       icon: defineComponent({
         render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+          h('svg', { fill: 'currentColor', viewBox: '0 0 16 16' }, [
             h('path', {
               'fill-rule': 'evenodd',
-              d: 'M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z',
+              d: 'M15.968,1.684a.338.338,0,0,0-.232-.253,1.192,1.192,0,0,0-.63.045S1.087,6.514.286,7.072c-.172.121-.23.19-.259.272-.138.4.293.573.293.573L3.933,9.094a.388.388,0,0,0,.183-.011c.822-.519,8.27-5.222,8.7-5.38.068-.02.118,0,.1.049-.172.6-6.606,6.319-6.641,6.354a.138.138,0,0,0-.049.118l-.337,3.528s-.142,1.1.956,0c.779-.779,1.527-1.425,1.9-1.738,1.242.858,2.579,1.806,3.156,2.3a1,1,0,0,0,.732.283.825.825,0,0,0,.7-.622S15.894,3.7,15.979,2.317C15.987,2.182,16,2.1,16,2A1.177,1.177,0,0,0,15.968,1.684Z',
               'clip-rule': 'evenodd',
             }),
           ]),
       }),
     },
     {
-      name: 'Dribbble',
-      href: '#',
+      name: 'YouTube',
+      href: 'https://www.youtube.com/c/staronkaby',
       icon: defineComponent({
         render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+          h('svg', { fill: 'currentColor', viewBox: '0 0 16 16' }, [
             h('path', {
               'fill-rule': 'evenodd',
-              d: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z',
+              d: 'M15.8,4.8c-0.2-1.3-0.8-2.2-2.2-2.4C11.4,2,8,2,8,2S4.6,2,2.4,2.4C1,2.6,0.3,3.5,0.2,4.8C0,6.1,0,8,0,8 s0,1.9,0.2,3.2c0.2,1.3,0.8,2.2,2.2,2.4C4.6,14,8,14,8,14s3.4,0,5.6-0.4c1.4-0.3,2-1.1,2.2-2.4C16,9.9,16,8,16,8S16,6.1,15.8,4.8z M6,11V5l5,3L6,11z',
               'clip-rule': 'evenodd',
             }),
           ]),
